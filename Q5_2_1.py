5c33b6dd1681e8d10d8075a680f51279bd29484b
k-matayosi <n24015@std.it-college.ac.jp>
2024-07-25 05:46:56 +0000

    kadai

---
+++ Q5_2_1.py
@@ -0,0 +1,8 @@
+tack = []
+stack.append(1)
+print('stack:', stack)
+stack.append(2)
+print('stack:', stack)
+print('pop 1 value:', stack.pop())
+print('pop 2 value:', stack.pop())
+print('stack:', stack)
